5d66e13050ecbd53ffb274a261bce85e5b8e9e47
YYuliia1102 <reya1102@gmail.com>
2023-04-28 17:25:47 +0300

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const liArray = [];
+
+// Звернаємось до классу gallery
+const galleryMarkup = galleryItems.map(item => `
+<div class="gallery__item">
+<a class="gallery__link" href="${item.original}">
+<img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/>
+</a>
+</div>
+`).join('');
+
+const gallery = document.querySelector('.gallery');
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+new SimpleLightbox('.gallery a',{
+    captionDelay: 250,
+    captionsData: 'alt',
+});
 
 console.log(galleryItems);
